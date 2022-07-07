806b3b628b97f30cad6d22be6373e9cb644fce9d
Thomas <thomas.gu25@gmail.com>
2022-07-07 17:26:54 +0800

    C0

---
+++ app/src/main/java/com/gu/example5_1gitvc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gu.example5_1gitvc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
